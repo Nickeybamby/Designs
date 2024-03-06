b174b94865f4e26d3541bd1f7eaec1fba8564f1e
nickeybamby <nicholasobazei@gmail.com>
2024-03-06 20:01:39 +0100

    added the navigation for a logged in user

---
+++ nav_loggedin.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/appointment_vitals.css">
+    <link rel="stylesheet" href="css/appointment.css">
+    <link rel="stylesheet" href="css/assessment_home.css">
+    <link rel="stylesheet" href="css/assessment.css">
+    <link rel="stylesheet" href="css/contact.css">
+    <link rel="stylesheet" href="css/eval_assessment.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="css/nav_loggedin.css">
+    <link rel="stylesheet" href="css/nav.css">
+    <link rel="stylesheet" href="css/registration.css">
+    <link rel="stylesheet" href="css/vitalsigns.css">
+    
+    
+    <title>VitalGuard Clinic</title>
+</head>
+<body>
+    <header>
+        <nav class="navbar">
+            <img src="images/logo1.png" alt="image of two hands holding a sprouting plant">
+
+            <ul class="nav-menu">
+                <li class="nav-item">
+                    <a href="index2.php" class="nav-link">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">About</a>
+                </li>
+                <li class="nav-item">
+                    <a href="contact.php" class="nav-link">Contact</a>
+                </li>
+                <li class="nav-item">
+                    <a href="assessment_home.php" class="nav-link">Assessment Forms</a>
+                </li>
+                <li class="nav-item">
+                    <a href="landing.php" class="nav-link">Sign out</a>
+                </li>
+            </ul>
+            <div class="hamburger">
+                <span class="bar"></span>
+                <span class="bar"></span>
+                <span class="bar"></span>
+            </div>
+
+        </nav>
+    </header>
+    <script src="scripts/nav.js"></script>
+</body>
+</html>
